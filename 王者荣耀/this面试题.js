723ef52c5badab982cdd096808edfbbb92e8c85b
yangzeng-cell <1015252164yangzeng@gmail.com>
2022-06-10 13:56:21 +0800

    update 20220610

--- 王者荣耀/this面试题.js
+++ 王者荣耀/this面试题.js
@@ -1,17 +1,36 @@
+// var name = "window";
+// var person = {
+//   name: "person",
+//   sayName() {
+//     console.log(this.name);
+//   },
+// };
+
+// function sayName() {
+//   var sss = person.sayName;
+//   sss();
+//   person.sayName();
+//   person.sayName();
+//   (b = person.sayName)();
+// }
+
+// sayName();
+
 var name = "window";
-var person = {
-  name: "person",
-  sayName() {
+var person1 = {
+  name: "person1",
+  foo1() {
     console.log(this.name);
   },
+  foo2: () => console.log(this.name),
+  foo3() {
+    return function () {
+      console.log(this.name);
+    };
+  },
+  foo4() {
+    return () => {
+      console.log(this.name);
+    };
+  },
 };
-
-function sayName() {
-  var sss = person.sayName;
-  sss();
-  person.sayName();
-  person.sayName();
-  (b = person.sayName)();
-}
-
-sayName();
